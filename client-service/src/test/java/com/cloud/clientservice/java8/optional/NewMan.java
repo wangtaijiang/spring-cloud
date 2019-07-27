a190140f1d1c19f259a9daf2b5740da2cdacf75f
superH <1042086797@qq.com>
2019-07-27 19:36:18 +0800

    Optional

---
+++ client-service/src/test/java/com/cloud/clientservice/java8/optional/NewMan.java
@@ -0,0 +1,8 @@
+package com.cloud.clientservice.java8.optional;
+
+/**
+ * @authur wtj
+ * @date 2019/7/27    19:21
+ */
+public class NewMan {
+}
---
+++ client-service/src/test/java/com/cloud/clientservice/java8/optional/TestOptional.java
@@ -0,0 +1,8 @@
+package com.cloud.clientservice.java8.optional;
+
+/**
+ * @authur wtj
+ * @date 2019/7/27    18:36
+ */
+public class TestOptional {
+}
